2bf44ce505d8a8998b912837b977d230a62171ff
Daniel Shechter <dshctr1993@gmail.com>
2022-11-23 20:46:45 +0200

    upload to render

--- server.js
+++ server.js
@@ -18,7 +18,8 @@ if (process.env.NODE_ENV === "production") {
     origin: [
       "http://127.0.0.1:5173",
       "http://localhost:3030",
-      "http://127.0.0.1:5173/veganizeIt/",
+      "http://localhost:3000",
+      "http://127.0.0.1:5174/",
     ],
     credentials: true,
   }
---
+++ services/db-service.js
@@ -0,0 +1,7 @@
+// function connect(endpoint) {
+//   const url = `https://vegit-cf561-default-rtdb.europe-west1.firebasedatabase.app/${endpoint}.json`
+// }
+
+// //
+// //
+// // https://vegit-cf561-default-rtdb.europe-west1.firebasedatabase.app/ing
