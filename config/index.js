adeebb7e8390e6cafe5898a2bbb14bd834207544
Daniel Shechter <dshctr1993@gmail.com>
2022-11-27 16:19:39 +0200

    routing has checnaged2

--- config/index.js
+++ config/index.js
@@ -4,7 +4,6 @@ if (process.env.NODE_ENV === "production") {
   // we are in production - return the prod set of keys
   config = require("./prod")
 } else {
-  console.log(process.env)
   // we are in development - return the dev keys!!!
   // config = require('./dev')
   config = require("./dev")
